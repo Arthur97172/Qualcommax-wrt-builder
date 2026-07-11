e1687008e02f2af8da99d4a89be9337fd0eb161b
Arthur97172 <arthur97172@outlook.com>
2026-07-11 12:46:31 +0800

    Update immortalwrt-custom-packages.sh

--- shell/immortalwrt-custom-packages.sh
+++ shell/immortalwrt-custom-packages.sh
@@ -6,7 +6,7 @@
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-cpufreq luci-i18n-cpufreq-zh-cn"
 
 # adguardhome广告拦截 (adguardhome)
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-adguardhome"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES adguardhome luci-app-adguardhome luci-i18n-adguardhome-zh-cn"
 
 #晶晨宝盒 (amlogic)
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-amlogic luci-i18n-amlogic-zh-cn"
